f41e42c1af31fe66188595de1af715b7409899a8
Onee_Only <kimww0306@gmail.com>
2023-02-25 11:45:45 +0900

    cronjob 생성

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/oneecoin/cronjob
 
 go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/robfig/cron"
 )
 
 // "* 0 * * *"
 func main() {
+	godotenv.Load()
 	url := "https://oneecoin-explorer-server.onrender.com/cron?secretKey=" + os.Getenv("SECRET_KEY")
-	http.Get(url)
+
+	c := cron.New()
+
+	c.AddFunc("@midnight", func() {
+		http.Get(url)
+	})
+
+	c.Start()
 }
